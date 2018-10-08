a733e683d53d527e2fae6d33485a7754fccb1a03
Louis Cospain <elcoosp@gmail.com>
2018-10-08 21:35:43 +0200

    test: 💍 Add remaining tests for current methods

--- src/test.ts
+++ src/test.ts
@@ -2,7 +2,7 @@ import Quuu, { IQueue } from './index'
 
 const setup = () => {
 	const a: IQueue = Quuu(1, 2, 3)
-	const b: IQueue = Quuu(1, 2, 4, 6)
+	const b: IQueue = Quuu(1, 2, 3)
 	const c: IQueue = Quuu(1, 2, 3)
 
 	return { a, b, c }
@@ -40,5 +40,43 @@ describe('should follow static-land spec', () => {
 
 		expect(rightId).toBe(true)
 		expect(leftId).toBe(true)
+	})
+})
+
+describe('Methods and props', () => {
+	it('iterator protocol', () => {
+		const a: IQueue = Quuu(1, 2, 4, 6)
+
+		expect([...a[Symbol.iterator]()]).toEqual([1, 2, 4, 6])
+	})
+
+	it('length', () => {
+		const a: IQueue = Quuu(1, 2, 4, 6)
+
+		expect(a.length).toBe(4)
+	})
+	it('isEmpty', () => {
+		const a: IQueue = Quuu()
+		const b: IQueue = Quuu(1, 3)
+		expect(a.isEmpty()).toBe(true)
+		expect(b.isEmpty()).toBe(false)
+	})
+	it('peek', () => {
+		const a: IQueue = Quuu(1, 3)
+		const b: IQueue = Quuu()
+		expect(a.peek()).toBe(1)
+		expect(b.peek()).toBe(null)
+	})
+
+	it('enqueue', () => {
+		const b: IQueue = Quuu(1).enqueue(3)
+
+		expect([...b[Symbol.iterator]()]).toEqual([1, 3])
+	})
+
+	it('dequeue', () => {
+		const b: IQueue = Quuu(1, 2).dequeue()
+
+		expect([...b[Symbol.iterator]()]).toEqual([2])
 	})
 })
